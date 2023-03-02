8b489a3844f05077738e2d028793dcbc61c456b3
RJ Green <rjgreen97@gmail.com>
2023-03-01 17:15:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+ResNeXt implimentaion written from scratch using PyTorch.
+
+https://arxiv.org/pdf/2201.03545.pdf
---
+++ resnext.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+
+class ResNeXt(nn.Module):
+    def __init__(self):
+        super(ResNeXt, self).__init__
+
+    def _make_layer(self):
+        pass
+
+    def forward(self, x):
+        pass
