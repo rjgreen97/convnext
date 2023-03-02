31df47fad431345ce740c7e6519cc6dd32f5fed2
RJ Green <rjgreen97@gmail.com>
2023-03-01 17:18:07 -0700

    build class skeletons

--- resnext.py
+++ resnext.py
@@ -2,6 +2,14 @@ import torch
 import torch.nn as nn
 
 
+class InvertedBottleneck(nn.Module):
+    def __init__(self):
+        super(InvertedBottleneck, self).__init__
+
+    def forward(self, x):
+        pass
+
+
 class ResNeXt(nn.Module):
     def __init__(self):
         super(ResNeXt, self).__init__
