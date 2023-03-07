eb13688fd23b865b0860b7ff67b83d618983fb16
RJ Green <rjgreen97@gmail.com>
2023-03-07 14:00:34 -0700

    continue to flesh out training

--- training/train.py
+++ training/train.py
@@ -4,3 +4,30 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 from tqdm import tqdm
+
+from models.convnext import ConvNeXt, InvertedBottleneck
+from training.training_config import TrainingConfig
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(torch.cuda.get_device_name())
+print(torch.__version__)
+print(torch.version.cuda)
+
+config = TrainingConfig()
+
+model_checkpoint_path = config.model_checkpoint_path
+
+transforms = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=(0.49139968, 0.48215841, 0.44653091),
+            std=(0.24703223, 0.24348513, 0.26158784),
+        ),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandAugment(2, 5),  # unsupported by PIL
+        transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3)),
+    ]
+)
+
+model = ConvNeXt().to(device)
--- training/training_config.py
+++ training/training_config.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel
 class TrainingConfig(BaseModel):
     model_checkpoint_path: str = "./model.pth"
     data_root_dir: str = "./data"
-    batch_size: int = 4096
-    learning_rate: float = 0.004
-    weight_decay: float = 0.05
+    batch_size: int = 512
+    learning_rate: float = 0.0005
+    weight_decay: float = 0.0001
     num_epochs: int = 300
     patience: int = 10
