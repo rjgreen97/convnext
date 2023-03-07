40e1f133a589746d655a4dbdbd5f6e5f68edd06b
RJ Green <rjgreen97@gmail.com>
2023-03-07 14:23:15 -0700

    build training loop

--- training/train.py
+++ training/train.py
@@ -17,6 +17,8 @@ config = TrainingConfig()
 
 model_checkpoint_path = config.model_checkpoint_path
 
+model = ConvNeXt().to(device)
+
 transforms = transforms.Compose(
     [
         transforms.ToTensor(),
@@ -30,4 +32,88 @@ transforms = transforms.Compose(
     ]
 )
 
-model = ConvNeXt().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.AdamW(
+    model.parameters(), lr=config.learning_rate, weight_decay=config.weight_decay
+)
+
+
+def load_data():
+    train_dataset = torchvision.datasets.CIFAR10(
+        root=config.data_root_dir, train=True, download=True, transform=transforms
+    )
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=0
+    )
+    test_dataset = torchvision.datasets.CIFAR10(
+        root=config.data_root_dir, train=False, download=True, transform=transforms
+    )
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=config.batch_size, shuffle=False, num_workers=0
+    )
+    return train_dataloader, test_dataloader
+
+
+def train(train_dataloader):
+    model.train()
+    best_validation_accuracy = 0
+
+    for epoch in range(1, config.num_epochs):
+        loop = tqdm(train_dataloader)
+        for data in loop:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            loop.set_description(f"Epoch {epoch}/{config.num_epochs}")
+            loop.set_postfix(loss=loss.item())
+
+        print(f"Training Loss: {loss.item():.3f}")
+
+        accuracy = validate(test_dataloader)
+        if accuracy > best_validation_accuracy:
+            epochs_without_improvement = 0
+            torch.save(model.state_dict(), model_checkpoint_path)
+            best_validation_accuracy = accuracy
+        else:
+            epochs_without_improvement += 1
+            print(f"Epochs without improvement: {epochs_without_improvement}")
+            if epochs_without_improvement > config.patience:
+                print(f"Early stopping, patience of {config.patience} reached")
+                break
+    print("Finished Training")
+    print(f"Best validation accuracy: {best_validation_accuracy:.2f}")
+
+
+def validate(test_dataloader):
+    model.eval()
+    correct_predictions = 0
+    total_predictions = 0
+    with torch.no_grad():
+        for data in test_dataloader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            _, predicted = torch.max(outputs.data, 1)
+
+            total_predictions += labels.size(0)
+            correct_predictions += (predicted == labels).sum().item()
+        accuracy = 100 * correct_predictions / total_predictions
+    print(f"Validation Loss: {loss.item():.3f}")
+    print(f"Validation Accuracy: {accuracy:.2f}")
+    return accuracy
+
+
+train_dataloader, test_dataloader = load_data()
+train(train_dataloader)
