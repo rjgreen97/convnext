847c99b3ac2a532ec8e77bd8b501281b4cf58a1e
RJ Green <rjgreen97@gmail.com>
2023-03-07 13:03:17 -0700

    get downsampling layers seperate and working

--- convnext.py
+++ convnext.py
@@ -10,8 +10,8 @@ class InvertedBottleneck(nn.Module):
                 in_channels=channels,
                 out_channels=channels,
                 kernel_size=7,
-                stride=3,
-                padding=0,
+                stride=1,
+                padding=3,
                 groups=channels,
             ),
             nn.GroupNorm(num_groups=1, num_channels=channels),
@@ -38,6 +38,7 @@ class InvertedBottleneck(nn.Module):
         x = self.block(x)
         if self.skip_connection:
             residual = self.skip_connection(residual)
+        x += residual
         return x
 
 
@@ -46,44 +47,56 @@ class ConvNeXt(nn.Module):
         self, channels=[96, 192, 384, 768], layer_depths=[3, 3, 9, 3], num_classes=10
     ):
         super(ConvNeXt, self).__init__()
-        self.patchify = nn.Conv2d(
-            in_channels=3,
-            out_channels=channels[0],
-            kernel_size=4,
-            stride=4,
-            padding=0,
+        self.patchify = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=channels[0],
+                kernel_size=4,
+                stride=4,
+                padding=0,
+            ),
+            nn.GroupNorm(num_groups=1, num_channels=channels[0]),
         )
         self.layer1 = self._make_layer(
             channels=channels[0], layer_depth=layer_depths[0]
         )
+        self.downsample1 = nn.Sequential(
+            nn.Sequential(
+                nn.GroupNorm(num_groups=1, num_channels=channels[0]),
+                nn.Conv2d(channels[0], channels[1], kernel_size=2, stride=2),
+            )
+        )
         self.layer2 = self._make_layer(
             channels=channels[1], layer_depth=layer_depths[1]
+        )
+        self.downsample2 = nn.Sequential(
+            nn.Sequential(
+                nn.GroupNorm(num_groups=1, num_channels=channels[1]),
+                nn.Conv2d(channels[1], channels[2], kernel_size=2, stride=2),
+            )
         )
         self.layer3 = self._make_layer(
             channels=channels[2], layer_depth=layer_depths[2]
         )
+        self.downsample3 = nn.Sequential(
+            nn.Sequential(
+                nn.GroupNorm(num_groups=1, num_channels=channels[2]),
+                nn.Conv2d(channels[2], channels[3], kernel_size=2, stride=2),
+            )
+        )
         self.layer4 = self._make_layer(
             channels=channels[3], layer_depth=layer_depths[3]
         )
-        self.classification_head = nn.Sequential(
+        self.head = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)),
-            nn.LayerNorm(normalized_shape=channels[3]),
+            nn.GroupNorm(num_groups=1, num_channels=channels[3]),
             nn.Linear(channels[3], num_classes),
         )
 
-    # method that creates a layer of inverted bottleneck blocks with skip connections and downsampling once layer depth is reached
     def _make_layer(self, channels, layer_depth):
         layers = []
-        for i in range(layer_depth):
-            if i == 0:
-                layers.append(
-                    InvertedBottleneck(
-                        channels=channels,
-                        skip_connection=nn.Conv2d(channels, channels, 1, 2),
-                    )
-                )
-            else:
-                layers.append(InvertedBottleneck(channels=channels))
+        for i in range(1, layer_depth):
+            layers.append(InvertedBottleneck(channels=channels))
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -92,15 +105,26 @@ class ConvNeXt(nn.Module):
         assert x.shape == (batch_size, 3, 256, 256)
         x = self.patchify(x)
         assert x.shape == (batch_size, 96, 64, 64)
-        # x = self.layer1(x)
-        # x = self.layer2(x)
-        # x = self.layer3(x)
-        # x = self.layer4(x)
-        # x = self.classification_head(x)
+        x = self.layer1(x)
+        assert x.shape == (batch_size, 96, 64, 64)
+        x = self.downsample1(x)
+        assert x.shape == (batch_size, 192, 32, 32)
+        x = self.layer2(x)
+        assert x.shape == (batch_size, 192, 32, 32)
+        x = self.downsample2(x)
+        assert x.shape == (batch_size, 384, 16, 16)
+        x = self.layer3(x)
+        assert x.shape == (batch_size, 384, 16, 16)
+        x = self.downsample3(x)
+        assert x.shape == (batch_size, 768, 8, 8)
+        x = self.layer4(x)
+        assert x.shape == (batch_size, 768, 8, 8)
+        x = torch.flatten(x, start_dim=1)
+        x = self.head(x)
         # return x
 
 
 if __name__ == "__main__":
     model = ConvNeXt()
-    x = torch.FloatTensor(4096, 3, 256, 256)
+    x = torch.FloatTensor(1, 3, 256, 256)
     model.forward(x)
