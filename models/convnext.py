5c96b2edf518d2d4c13922338c9b0f9a94fba200
RJ Green <rjgreen97@gmail.com>
2023-03-08 11:35:17 -0600

    update downsampling method

--- models/convnext.py
+++ models/convnext.py
@@ -44,7 +44,7 @@ class ConvNeXt(nn.Module):
         self, channels=[96, 192, 384, 768], layer_depths=[3, 3, 9, 3], num_classes=10
     ):
         super(ConvNeXt, self).__init__()
-        self.patchify = nn.Sequential(
+        self.stem = nn.Sequential(
             nn.Conv2d(
                 in_channels=3,
                 out_channels=channels[0],
@@ -57,17 +57,14 @@ class ConvNeXt(nn.Module):
         self.layer1 = self._make_conv_layer(
             channels=channels[0], layer_depth=layer_depths[0]
         )
-        self.downsample1 = self._make_downsample_layer(channels=channels[0])
         self.layer2 = self._make_conv_layer(
             channels=channels[1], layer_depth=layer_depths[1]
         )
-        self.downsample2 = self._make_downsample_layer(channels=channels[1])
         self.layer3 = self._make_conv_layer(
             channels=channels[2], layer_depth=layer_depths[2]
         )
-        self.downsample3 = self._make_downsample_layer(channels=channels[2])
         self.layer4 = self._make_conv_layer(
-            channels=channels[3], layer_depth=layer_depths[3]
+            channels=channels[3], layer_depth=layer_depths[3], add_downsample=False
         )
         self.head = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)),
@@ -76,35 +73,25 @@ class ConvNeXt(nn.Module):
             nn.Linear(channels[3], num_classes),
         )
 
-    def _make_conv_layer(self, channels, layer_depth):
+    def _make_conv_layer(self, channels, layer_depth, add_downsample=True):
         layers = []
         for _i in range(1, layer_depth):
             layers.append(InvertedBottleneck(channels=channels))
+        if add_downsample:
+            layers.append(nn.GroupNorm(num_groups=1, num_channels=channels))
+            layers.append(nn.Conv2d(channels, channels * 2, kernel_size=2, stride=2))
         return nn.Sequential(*layers)
-
-    def _make_downsample_layer(self, channels):
-        return nn.Sequential(
-            nn.GroupNorm(num_groups=1, num_channels=channels),
-            nn.Conv2d(channels, channels * 2, kernel_size=2, stride=2),
-        )
 
     def forward(self, x):
         batch_size = x.shape[0]
-
         assert x.shape == (batch_size, 3, 256, 256)
-        x = self.patchify(x)
+        x = self.stem(x)
         assert x.shape == (batch_size, 96, 64, 64)
         x = self.layer1(x)
-        assert x.shape == (batch_size, 96, 64, 64)
-        x = self.downsample1(x)
         assert x.shape == (batch_size, 192, 32, 32)
         x = self.layer2(x)
-        assert x.shape == (batch_size, 192, 32, 32)
-        x = self.downsample2(x)
         assert x.shape == (batch_size, 384, 16, 16)
         x = self.layer3(x)
-        assert x.shape == (batch_size, 384, 16, 16)
-        x = self.downsample3(x)
         assert x.shape == (batch_size, 768, 8, 8)
         x = self.layer4(x)
         assert x.shape == (batch_size, 768, 8, 8)
